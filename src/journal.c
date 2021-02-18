9051fcf6163ce10cfa3c5a40dfe9860d2d499695
markuspg <markuspg@users.noreply.github.com>
2021-02-18 19:17:46 +0100

    Re-enable help pop-up on hover over link

--- src/journal.c
+++ src/journal.c
@@ -973,76 +973,79 @@ hover_loose_focus(GtkWidget *w, GdkEventFocus *ev, gpointer data)
 	return 0;
 }
 
-/* static void
-html_on_url_cb(GtkHTML *doc, const gchar *url, gpointer data)
+static void
+html_on_url_cb(WebKitWebView *web_view, const gchar *title, const gchar *uri,
+               gpointer data)
 {
-  Wiggy *wig = data;
-  if (NULL == wig->top)
-        return; */
+	Wiggy *wig = data;
+	if (NULL == wig->top)
+	{
+		return;
+	}
 
-/* Create and initialize the hover-help window */
-/* if (!wig->hover_help_window)
-{
-wig->hover_help_window = gtk_window_new(GTK_WINDOW_POPUP);
-GtkWindow *wino = GTK_WINDOW(wig->hover_help_window);
-gtk_window_set_decorated(wino, FALSE);
-gtk_window_set_destroy_with_parent(wino, TRUE);
-gtk_window_set_transient_for(wino, GTK_WINDOW(wig->top));
-// gtk_window_set_type_hint (wino, GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
-gtk_window_set_resizable(wino, FALSE); /* FALSE to enable auto-resize */
+	/* Create and initialize the hover-help window */
+	if (!wig->hover_help_window)
+	{
+		wig->hover_help_window = gtk_window_new(GTK_WINDOW_POPUP);
+		GtkWindow *wino = GTK_WINDOW(wig->hover_help_window);
+		gtk_window_set_decorated(wino, FALSE);
+		gtk_window_set_destroy_with_parent(wino, TRUE);
+		gtk_window_set_transient_for(wino, GTK_WINDOW(wig->top));
+		// gtk_window_set_type_hint (wino, GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
+		gtk_window_set_resizable(wino, FALSE); /* FALSE to enable auto-resize */
 
-/* There must be a better way to draw a line around the box ?? */
-/*GtkWidget *frame = gtk_frame_new(NULL);
-gtk_container_add(GTK_CONTAINER(wino), frame);
-gtk_container_set_resize_mode(GTK_CONTAINER(frame), GTK_RESIZE_PARENT);
-gtk_widget_show(frame); */
+		/* There must be a better way to draw a line around the box ?? */
+		GtkWidget *frame = gtk_frame_new(NULL);
+		gtk_container_add(GTK_CONTAINER(wino), frame);
+		gtk_container_set_resize_mode(GTK_CONTAINER(frame), GTK_RESIZE_PARENT);
+		gtk_widget_show(frame);
 
-/* There must be a better way to pad the text all around ?? */
-/* GtkWidget *align = gtk_alignment_new(0.5, 0.5, 1.0, 1.0); */
-// gtk_alignment_set_padding (GTK_ALIGNMENT(align), 6, 6, 6, 6);
-/* gtk_container_add(GTK_CONTAINER(frame), align);
-gtk_container_set_resize_mode(GTK_CONTAINER(align), GTK_RESIZE_PARENT);
-gtk_widget_show(align);
+		/* There must be a better way to pad the text all around ?? */
+		GtkWidget *align = gtk_alignment_new(0.5, 0.5, 1.0, 1.0);
+		// gtk_alignment_set_padding (GTK_ALIGNMENT(align), 6, 6, 6, 6);
+		gtk_container_add(GTK_CONTAINER(frame), align);
+		gtk_container_set_resize_mode(GTK_CONTAINER(align), GTK_RESIZE_PARENT);
+		gtk_widget_show(align);
 
-GtkWidget *label = gtk_label_new("xxx");
-wig->hover_label = GTK_LABEL(label);
-gtk_container_add(GTK_CONTAINER(align), label);
-gtk_widget_show(label); */
+		GtkWidget *label = gtk_label_new("xxx");
+		wig->hover_label = GTK_LABEL(label);
+		gtk_container_add(GTK_CONTAINER(align), label);
+		gtk_widget_show(label);
 
-/* So that we can loose focus later */
-/* gtk_window_set_focus(GTK_WINDOW(wig->top), GTK_WIDGET(wig->html)); */
+		/* So that we can loose focus later */
+		gtk_window_set_focus(GTK_WINDOW(wig->top), GTK_WIDGET(wig->html));
 
-/* Set up in initial default, so later move works. */
-/* int px = 0, py = 0, rx = 0, ry = 0;
-gtk_widget_get_pointer(GTK_WIDGET(wig->top), &px, &py);
-gtk_window_get_position(GTK_WINDOW(wig->top), &rx, &ry);
-gtk_window_move(wino, rx + px, ry + py);
+		/* Set up in initial default, so later move works. */
+		int px = 0, py = 0, rx = 0, ry = 0;
+		gtk_widget_get_pointer(GTK_WIDGET(wig->top), &px, &py);
+		gtk_window_get_position(GTK_WINDOW(wig->top), &rx, &ry);
+		gtk_window_move(wino, rx + px, ry + py);
+	}
+
+	if (uri)
+	{
+		char *msg = get_hover_msg(uri);
+		gtk_label_set_markup(wig->hover_label, msg);
+		gtk_container_resize_children(GTK_CONTAINER(wig->hover_help_window));
+		gtk_container_check_resize(GTK_CONTAINER(wig->hover_help_window));
+		g_free(msg);
+	}
+
+	/* If hovering over a URL, bring up the help popup after one second. */
+	if (uri)
+	{
+		/* 600 milliseconds == 0.6 second */
+		wig->hover_timeout_id = gtk_timeout_add(600, hover_timer_func, wig);
+	} else
+	{
+		if (wig->hover_timeout_id)
+		{
+			gtk_timeout_remove(wig->hover_timeout_id);
+			wig->hover_timeout_id = 0;
+			gtk_widget_hide(wig->hover_help_window);
+		}
+	}
 }
-
-if (url)
-{
-char *msg = get_hover_msg(url);
-gtk_label_set_markup(wig->hover_label, msg);
-gtk_container_resize_children(GTK_CONTAINER(wig->hover_help_window));
-gtk_container_check_resize(GTK_CONTAINER(wig->hover_help_window));
-g_free(msg);
-} */
-
-/* If hovering over a URL, bring up the help popup after one second. */
-/* if (url)
-{ */
-/* 600 milliseconds == 0.6 second */
-/* wig->hover_timeout_id = gtk_timeout_add(600, hover_timer_func, wig);
-} else
-{
-if (wig->hover_timeout_id)
-{
-gtk_timeout_remove(wig->hover_timeout_id);
-wig->hover_timeout_id = 0;
-gtk_widget_hide(wig->hover_help_window);
-}
-}
-} */
 
 /* ============================================================== */
 /* HTML form (method=GET, POST) events */
@@ -1222,13 +1225,13 @@ g_signal_connect(G_OBJECT(wig->html), "submit", G_CALLBACK(submit_clicked_cb),
 	               wig);
 
 g_signal_connect(G_OBJECT(wig->html), "url_requested",
-	               G_CALLBACK(html_url_requested_cb), wig);
+	                 G_CALLBACK(html_url_requested_cb), wig); */
 
-g_signal_connect(G_OBJECT(wig->html), "on_url", G_CALLBACK(html_on_url_cb),
-	               wig);
+	g_signal_connect(G_OBJECT(wig->html), "hovering-over-link",
+	                 G_CALLBACK(html_on_url_cb), wig);
 
-g_signal_connect(G_OBJECT(wig->html), "focus_out_event",
-	                 G_CALLBACK(hover_loose_focus), wig); */
+	/* g_signal_connect(G_OBJECT(wig->html), "focus_out_event",
+	                   G_CALLBACK(hover_loose_focus), wig); */
 
 	gtk_widget_show(GTK_WIDGET(wig->html));
 	gtk_widget_show(jnl_top);

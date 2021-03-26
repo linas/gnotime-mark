af856812a648df234c8a9e98346df69d174f8892
markuspg <markuspg@users.noreply.github.com>
2021-03-27 00:49:37 +0100

    Replace gnome_help_display by gtk_show_uri

--- src/dialog.c
+++ src/dialog.c
@@ -16,34 +16,43 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <config.h>
-#include <gnome.h>
-#include <libgnome/gnome-help.h>
-
 #include "dialog.h"
 
-/* ================================================================= */
+#include <string.h>
 
 void
 gtt_help_popup(GtkWidget *widget, gpointer data)
 {
 	GError *err = NULL;
 	char *section = data;
-	if ((section != NULL) && !strcmp("", section))
-		section = NULL;
-	gnome_help_display("gnotime", section, &err);
-	if (err)
+	gchar *uri = NULL;
+
+	/* Determine the URI to be shown */
+	if ((NULL == section) || ((section != NULL) && !strcmp("", section)))
 	{
-		GtkWidget *mb;
-		mb = gtk_message_dialog_new(
+		section = NULL;
+		uri = g_strdup("ghelp:gnotime");
+	} else
+	{
+		uri = g_strdup_printf("ghelp:gnotime#%s", section);
+	}
+
+	/* Show the determined URI */
+	const gboolean res = gtk_show_uri(NULL, uri, GDK_CURRENT_TIME, &err);
+	g_free(uri);
+	uri = NULL;
+
+	/* Handle errors */
+	if ((FALSE == res) || (NULL != err))
+	{
+		GtkWidget *const mb = gtk_message_dialog_new(
 				GTK_IS_WINDOW(widget) ? GTK_WINDOW(widget) : NULL, GTK_DIALOG_MODAL,
 				GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "%s", err->message);
 		g_signal_connect(G_OBJECT(mb), "response", G_CALLBACK(gtk_widget_destroy),
 		                 mb);
 		gtk_widget_show(mb);
+		gtk_widget_destroy(mb);
 
 		printf("duude gnome help err msg: %s\n", err->message);
 	}
 }
-
-/* ==================== END OF FILE ================================ */

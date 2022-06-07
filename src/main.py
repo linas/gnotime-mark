6614c5fcceed0b9886d60410aea8381e7f9fa1e0
markuspg <markuspg@users.noreply.github.com>
2022-06-07 22:04:04 +0200

    Add GnoTime Python script stub

---
+++ src/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+################################################################################
+#
+# GnoTime - a to-do list organizer, diary and billing system
+#
+# Copyright (C) 1997,98 Eckehard Berns
+# Copyright (C) 2001 Linas Vepstas <linas@linas.org>
+# Copyright (C) 2010 Goedson Teixeira Paixao <goedson@debian.org>
+# Copyright (C) 2022      Markus Prasser
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+#
+################################################################################
+
+def main():
+    print("GnoTime")
+
+if __name__ == "__main__":
+    main()
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4

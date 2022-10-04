59c93281ff8c9c71077cdbc07361ada6bb62aaf2
markuspg <markuspg@users.noreply.github.com>
2022-10-04 22:49:17 +0200

    Format codebase accordingly

--- src/myoaf.c
+++ src/myoaf.c
@@ -5,9 +5,6 @@
    eliminated.
 */
 
-
-
-
 #include <gtk/gtk.h>
 #if 0
 #include <liboaf/liboaf.h>
@@ -16,10 +13,8 @@
 
 #include "myoaf.h"
 
-
-
 void
-edit_calendar(GtkWidget *w, gpointer data)
+edit_calendar (GtkWidget *w, gpointer data)
 {
 
 #if 0
@@ -94,5 +89,3 @@ srv->location_info, srv->hostname);
 printf ("duude query = %p item=%p\n", sl, cal);
 #endif
 }
-
-

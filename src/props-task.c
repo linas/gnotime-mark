fb76cdf424d5af74ace8afd9373937ad2bcbbec9
markuspg <markuspg@users.noreply.github.com>
2022-10-24 18:18:06 +0200

    Replace widget-related defines in props-task.c

--- src/props-task.c
+++ src/props-task.c
@@ -1,5 +1,6 @@
 /*   Task Properties for GTimeTracker - a time tracker
  *   Copyright (C) 2001,2002,2003,2004 Linas Vepstas <linas@linas.org>
+ * Copyright (C) 2022      Markus Prasser
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -55,6 +56,14 @@ typedef struct PropTaskDlg_s
 
 } PropTaskDlg;
 
+static GtkWidget *btagged (GtkWidget *wdgt, PropTaskDlg *dlg);
+static gint get_menu (GtkOptionMenu *wdgt, const gchar *name);
+static void mentry (GtkOptionMenu *wdgt, const gchar *name, guint order,
+                    gint val);
+static GtkOptionMenu *mugged (GtkWidget *wdgt, PropTaskDlg *dlg);
+static GtkWidget *ntagged (GtkWidget *wdgt, PropTaskDlg *dlg);
+static GtkWidget *texted (GtkWidget *wdgt, PropTaskDlg *dlg);
+
 /* ============================================================== */
 
 #define TSK_SETUP(dlg)                                                        \
@@ -69,14 +78,6 @@ typedef struct PropTaskDlg_s
 
 /* ============================================================== */
 /* Copy from widget to gtt objects */
-
-#define GET_MENU(WIDGET, NAME)                                                \
-  ({                                                                          \
-    GtkWidget *menu, *menu_item;                                              \
-    menu = gtk_option_menu_get_menu (WIDGET);                                 \
-    menu_item = gtk_menu_get_active (GTK_MENU (menu));                        \
-    (g_object_get_data (G_OBJECT (menu_item), NAME));                         \
-  })
 
 static void
 save_task_notes (GtkWidget *w, PropTaskDlg *dlg)
@@ -117,13 +118,13 @@ save_task_billinfo (GtkWidget *w, PropTaskDlg *dlg)
   ivl = (int)(60.0 * atof (gtk_entry_get_text (dlg->unit)));
   gtt_task_set_bill_unit (dlg->task, ivl);
 
-  status = (GttBillStatus)GET_MENU (dlg->billstatus, "billstatus");
+  status = (GttBillStatus)get_menu (dlg->billstatus, "billstatus");
   gtt_task_set_billstatus (dlg->task, status);
 
-  able = (GttBillable)GET_MENU (dlg->billable, "billable");
+  able = (GttBillable)get_menu (dlg->billable, "billable");
   gtt_task_set_billable (dlg->task, able);
 
-  rate = (GttBillRate)GET_MENU (dlg->billrate, "billrate");
+  rate = (GttBillRate)get_menu (dlg->billrate, "billrate");
   gtt_task_set_billrate (dlg->task, rate);
 
   dlg->ignore_events = FALSE;
@@ -226,54 +227,6 @@ destroy_cb (GtkWidget *w, PropTaskDlg *dlg)
 
 /* ============================================================== */
 
-#define NTAGGED(NAME)                                                         \
-  ({                                                                          \
-    GtkWidget *widget;                                                        \
-    widget = glade_xml_get_widget (gtxml, NAME);                              \
-    g_signal_connect (G_OBJECT (widget), "changed",                           \
-                      G_CALLBACK (save_task_notes), dlg);                     \
-    widget;                                                                   \
-  })
-
-#define BTAGGED(NAME)                                                         \
-  ({                                                                          \
-    GtkWidget *widget;                                                        \
-    widget = glade_xml_get_widget (gtxml, NAME);                              \
-    g_signal_connect (G_OBJECT (widget), "changed",                           \
-                      G_CALLBACK (save_task_billinfo), dlg);                  \
-    widget;                                                                   \
-  })
-
-#define TEXTED(NAME)                                                          \
-  ({                                                                          \
-    GtkWidget *widget;                                                        \
-    GtkTextBuffer *buff;                                                      \
-    widget = glade_xml_get_widget (gtxml, NAME);                              \
-    buff = gtk_text_view_get_buffer (GTK_TEXT_VIEW (widget));                 \
-    g_signal_connect (G_OBJECT (buff), "changed",                             \
-                      G_CALLBACK (save_task_notes), dlg);                     \
-    widget;                                                                   \
-  })
-
-#define MUGGED(NAME)                                                          \
-  ({                                                                          \
-    GtkWidget *widget, *mw;                                                   \
-    widget = glade_xml_get_widget (gtxml, NAME);                              \
-    mw = gtk_option_menu_get_menu (GTK_OPTION_MENU (widget));                 \
-    g_signal_connect (G_OBJECT (mw), "selection_done",                        \
-                      G_CALLBACK (save_task_billinfo), dlg);                  \
-    GTK_OPTION_MENU (widget);                                                 \
-  })
-
-#define MENTRY(WIDGET, NAME, ORDER, VAL)                                      \
-  {                                                                           \
-    GtkWidget *menu_item;                                                     \
-    GtkMenu *menu = GTK_MENU (gtk_option_menu_get_menu (WIDGET));             \
-    gtk_option_menu_set_history (WIDGET, ORDER);                              \
-    menu_item = gtk_menu_get_active (menu);                                   \
-    g_object_set_data (G_OBJECT (menu_item), NAME, (gpointer)VAL);            \
-  }
-
 static PropTaskDlg *
 prop_task_dialog_new (void)
 {
@@ -301,29 +254,33 @@ prop_task_dialog_new (void)
   /* ------------------------------------------------------ */
   /* grab the various entry boxes and hook them up */
 
-  dlg->memo = GTK_ENTRY (NTAGGED ("memo box"));
-  dlg->notes = GTK_TEXT_VIEW (TEXTED ("notes box"));
+  dlg->memo
+      = GTK_ENTRY (ntagged (glade_xml_get_widget (gtxml, "memo box"), dlg));
+  dlg->notes = GTK_TEXT_VIEW (
+      texted (glade_xml_get_widget (gtxml, "notes box"), dlg));
 
-  dlg->billstatus = MUGGED ("billstatus menu");
-  dlg->billable = MUGGED ("billable menu");
-  dlg->billrate = MUGGED ("billrate menu");
-  dlg->unit = GTK_ENTRY (BTAGGED ("unit box"));
+  dlg->billstatus
+      = mugged (glade_xml_get_widget (gtxml, "billstatus menu"), dlg);
+  dlg->billable = mugged (glade_xml_get_widget (gtxml, "billable menu"), dlg);
+  dlg->billrate = mugged (glade_xml_get_widget (gtxml, "billrate menu"), dlg);
+  dlg->unit
+      = GTK_ENTRY (btagged (glade_xml_get_widget (gtxml, "unit box"), dlg));
 
   /* ------------------------------------------------------ */
   /* associate values with the three option menus */
 
-  MENTRY (dlg->billstatus, "billstatus", 0, GTT_HOLD);
-  MENTRY (dlg->billstatus, "billstatus", 1, GTT_BILL);
-  MENTRY (dlg->billstatus, "billstatus", 2, GTT_PAID);
+  mentry (dlg->billstatus, "billstatus", 0, GTT_HOLD);
+  mentry (dlg->billstatus, "billstatus", 1, GTT_BILL);
+  mentry (dlg->billstatus, "billstatus", 2, GTT_PAID);
 
-  MENTRY (dlg->billable, "billable", 0, GTT_BILLABLE);
-  MENTRY (dlg->billable, "billable", 1, GTT_NOT_BILLABLE);
-  MENTRY (dlg->billable, "billable", 2, GTT_NO_CHARGE);
+  mentry (dlg->billable, "billable", 0, GTT_BILLABLE);
+  mentry (dlg->billable, "billable", 1, GTT_NOT_BILLABLE);
+  mentry (dlg->billable, "billable", 2, GTT_NO_CHARGE);
 
-  MENTRY (dlg->billrate, "billrate", 0, GTT_REGULAR);
-  MENTRY (dlg->billrate, "billrate", 1, GTT_OVERTIME);
-  MENTRY (dlg->billrate, "billrate", 2, GTT_OVEROVER);
-  MENTRY (dlg->billrate, "billrate", 3, GTT_FLAT_FEE);
+  mentry (dlg->billrate, "billrate", 0, GTT_REGULAR);
+  mentry (dlg->billrate, "billrate", 1, GTT_OVERTIME);
+  mentry (dlg->billrate, "billrate", 2, GTT_OVEROVER);
+  mentry (dlg->billrate, "billrate", 3, GTT_FLAT_FEE);
 
   dlg->ignore_events = FALSE;
   dlg->task_freeze = FALSE;
@@ -366,6 +323,60 @@ prop_task_dialog_show (GttTask *task)
   gtt_project_add_notifier (prj, redraw, global_dlog);
 
   gtk_widget_show (GTK_WIDGET (global_dlog->dlg));
+}
+
+static GtkWidget *
+btagged (GtkWidget *const wdgt, PropTaskDlg *const dlg)
+{
+  g_signal_connect (G_OBJECT (wdgt), "changed",
+                    G_CALLBACK (save_task_billinfo), dlg);
+  return wdgt;
+}
+
+static gint
+get_menu (GtkOptionMenu *wdgt, const gchar *name)
+{
+  GtkWidget *const menu = gtk_option_menu_get_menu (wdgt);
+  GtkWidget *const menu_item = gtk_menu_get_active (GTK_MENU (menu));
+  return GPOINTER_TO_INT (g_object_get_data (G_OBJECT (menu_item), name));
+}
+
+static void
+mentry (GtkOptionMenu *const wdgt, const gchar *const name, const guint order,
+        const gint val)
+{
+  GtkMenu *const menu = GTK_MENU (gtk_option_menu_get_menu (wdgt));
+  gtk_option_menu_set_history (wdgt, order);
+  GtkWidget *const menu_item = gtk_menu_get_active (menu);
+  g_object_set_data (G_OBJECT (menu_item), name, GINT_TO_POINTER (val));
+}
+
+static GtkOptionMenu *
+mugged (GtkWidget *const wdgt, PropTaskDlg *const dlg)
+{
+  GtkWidget *const mw = gtk_option_menu_get_menu (GTK_OPTION_MENU (wdgt));
+  g_signal_connect (G_OBJECT (mw), "selection_done",
+                    G_CALLBACK (save_task_billinfo), dlg);
+  return GTK_OPTION_MENU (wdgt);
+}
+
+static GtkWidget *
+ntagged (GtkWidget *const wdgt, PropTaskDlg *const dlg)
+{
+  g_signal_connect (G_OBJECT (wdgt), "changed", G_CALLBACK (save_task_notes),
+                    dlg);
+  return wdgt;
+}
+
+static GtkWidget *
+texted (GtkWidget *const wdgt, PropTaskDlg *const dlg)
+{
+  GtkTextBuffer *const tvb = gtk_text_view_get_buffer (GTK_TEXT_VIEW (wdgt));
+
+  g_signal_connect (G_OBJECT (tvb), "changed", G_CALLBACK (save_task_notes),
+                    dlg);
+
+  return wdgt;
 }
 
 /* ===================== END OF FILE =========================== */
